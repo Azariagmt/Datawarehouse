fd0fa1df04a8a3fad9850d08d2f6d37a677ec471
Azariagmt <azariagebremichael10@gmail.com>
2021-10-09 12:55:52 +0300

    Airflow: edits required data  loading DAG

--- dags/load_required_data_dag.py
+++ dags/load_required_data_dag.py
@@ -3,7 +3,7 @@ from airflow.operators.mysql_operator import MySqlOperator
 
 default_arg = {'owner': 'airflow', 'start_date': '2020-02-28'}
 
-dag = DAG('load-data',
+dag = DAG('load-essential-data',
           default_args=default_arg,
           schedule_interval='@once',
           template_searchpath=['/usr/local/airflow/include/'])
@@ -19,6 +19,15 @@ load_I80_stations = MySqlOperator(dag=dag,
                            task_id='load_stations',
                             sql='load_I80_stations.sql')
 
+create_station_summary = MySqlOperator(dag=dag,
+                mysql_conn_id='mysql-connect',
+                task_id='create_station_summary',
+                sql='create_station_summary.sql')
+
+load_station_summary = MySqlOperator(dag=dag,
+                           mysql_conn_id='mysql-connect', 
+                           task_id='load_station_summary',
+                            sql='load_station_summary.sql')
 
 
-create_I80_stations >> load_I80_stations 
+create_I80_stations >> load_I80_stations >> create_station_summary >> load_station_summary
